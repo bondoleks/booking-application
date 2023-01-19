43bf20792014e2b420b2942f4545bb95a04416db
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2023-01-19 14:15:51 +0200

    BitOperation

--- booking-application/src/main/java/Main.java
+++ booking-application/src/main/java/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println(isOverflowWhenAdd(Integer.MAX_VALUE, 3));
+            System.out.println(isOverflowWhenSub(Integer.MIN_VALUE, -3));
+            System.out.println(isOverflowWhenMul(Integer.MIN_VALUE, 3));
+        }
+
+        public static boolean isOverflowWhenAdd(int x, int y) {
+            boolean result;
+            return result = (x + y) > Integer.MAX_VALUE << 1;
+        }
+
+        public static boolean isOverflowWhenSub(int x, int y) {
+            boolean result;
+            return result = (x - y) > Integer.MIN_VALUE >> 1;
+        }
+
+        public static boolean isOverflowWhenMul(int x, int y) {
+            boolean result;
+            return result = (x * y) > Integer.MAX_VALUE << 1;
+        }
     }
-}
